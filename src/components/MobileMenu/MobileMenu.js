7d11db2d8227b0285a53c3e41fb15295b32d20bc
Wicho <54341712+Wichosu@users.noreply.github.com>
2024-09-04 13:42:37 -0600

    Exercise 4 done

--- src/components/MobileMenu/MobileMenu.js
+++ src/components/MobileMenu/MobileMenu.js
@@ -3,6 +3,8 @@ import React from 'react';
 import styled from 'styled-components/macro';
 import { DialogOverlay, DialogContent } from '@reach/dialog';
 
+import { COLORS } from '../../constants';
+
 import UnstyledButton from '../UnstyledButton';
 import Icon from '../Icon';
 import VisuallyHidden from '../VisuallyHidden';
@@ -13,23 +15,87 @@ const MobileMenu = ({ isOpen, onDismiss }) => {
   }
 
   return (
-    <div>
-      <button onClick={onDismiss}>Dismiss menu</button>
-      <nav>
-        <a href="/sale">Sale</a>
-        <a href="/new">New&nbsp;Releases</a>
-        <a href="/men">Men</a>
-        <a href="/women">Women</a>
-        <a href="/kids">Kids</a>
-        <a href="/collections">Collections</a>
-      </nav>
-      <footer>
-        <a href="/terms">Terms and Conditions</a>
-        <a href="/privacy">Privacy Policy</a>
-        <a href="/contact">Contact Us</a>
-      </footer>
-    </div>
+    <Wrapper>
+      <FlexWrapper>
+        <Button onClick={onDismiss}>
+          <Icon id={'close'} color={COLORS.gray[900]} size={'24px'} strokeWidth={'2px'} />
+        </Button>
+        <Nav>
+          <Link href="/sale">Sale</Link>
+          <Link href="/new">New&nbsp;Releases</Link>
+          <Link href="/men">Men</Link>
+          <Link href="/women">Women</Link>
+          <Link href="/kids">Kids</Link>
+          <Link href="/collections">Collections</Link>
+        </Nav>
+        <Footer>
+          <Link href="/terms">Terms and Conditions</Link>
+          <Link href="/privacy">Privacy Policy</Link>
+          <Link href="/contact">Contact Us</Link>
+        </Footer>
+      </FlexWrapper>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled(DialogOverlay)`
+  background-color: ${COLORS.fade};
+  height: 100%;
+  width: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+`;
+
+const FlexWrapper = styled(DialogContent)`
+  position: absolute;
+  top: 0;
+  right: 0;
+  background-color: ${COLORS.white};
+  display: flex;
+  flex-direction: column;
+  padding: 26px 16px 32px 32px;
+  justify-content: space-between;
+  height: 100%;
+  width: 75%;
+`;
+
+const Button = styled.button`
+  background-color: transparent;
+  border: none;
+  margin-left: auto;
+`;
+
+const Nav = styled.nav`
+  display: flex;
+  flex-direction: column;
+  text-transform: uppercase;
+  gap: 22px;
+`;
+
+const Footer = styled.footer`
+  display: flex;
+  flex-direction: column;
+  gap: 14px;
+`;
+
+const Link = styled.a`
+  color: ${COLORS.gray[900]};
+  text-decoration: none;
+  width: fit-content;
+
+  &:hover {
+    color: ${COLORS.secondary};
+  }
+
+  &:focus {
+    color: ${COLORS.secondary};
+  }
+
+  ${Footer} & {
+    color: ${COLORS.gray[700]};
+  }
+`;
+
 
 export default MobileMenu;
